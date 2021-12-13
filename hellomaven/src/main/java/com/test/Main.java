0a4aae49fd6756b4b50f203651931568e525b120
Jiri Pinkas <jirka.pinkas@gmail.com>
2021-12-13 15:42:59 +0100

    + priklad na sonar & try-with-resources

--- hellomaven/src/main/java/com/test/Main.java
+++ hellomaven/src/main/java/com/test/Main.java
@@ -2,9 +2,18 @@ package com.test;
 
 import com.google.common.base.Strings;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        try (Stream<String> stream = Files.lines(Paths.get("pom.xml"))) {
+            stream.forEach(System.out::println);
+        }
         System.out.println(Strings.repeat("Hello World Maven! ", 3));
     }
 }
