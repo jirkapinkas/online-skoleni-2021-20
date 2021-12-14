43889eef1bb98cf41c047c6e6691f6e8599d16e0
Jiri Pinkas <jirka.pinkas@gmail.com>
2021-12-14 10:14:14 +0100

    + konfigurace logovani pomoci logback & slf4j

--- hellomaven/src/main/java/com/test/Main.java
+++ hellomaven/src/main/java/com/test/Main.java
@@ -1,15 +1,19 @@
 package com.test;
 
 import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+@Slf4j
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        log.info("logback works!");
 
         try (Stream<String> stream = Files.lines(Paths.get("pom.xml"))) {
             stream.forEach(System.out::println);
