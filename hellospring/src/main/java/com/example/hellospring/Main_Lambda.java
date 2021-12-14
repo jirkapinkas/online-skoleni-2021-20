cf84e698421433dcb3beb43ec360825704156600
Jiri Pinkas <jirka.pinkas@gmail.com>
2021-12-14 13:28:49 +0100

    + lambda vyrazy

--- hellospring/src/main/java/com/example/hellospring/Main_Lambda.java
+++ hellospring/src/main/java/com/example/hellospring/Main_Lambda.java
@@ -1,8 +1,10 @@
 package com.example.hellospring;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 // Jediny rozdil mezi interface a abstract class od Java 8:
 // Abstraktni trida muze mit stav!!! Inteface ne!!!
@@ -22,6 +24,40 @@ interface I1 {
 public class Main_Lambda {
 
     public static void main(String[] args) {
+        // anonymous class
+        Arrays.asList("a", "b", "c")
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
+
+        // lambda 1
+        Arrays.asList("a", "b", "c")
+                .forEach((String s) -> {
+                    System.out.println(s);
+                });
+
+        // lambda 2
+        Arrays.asList("a", "b", "c")
+                .forEach((s) -> {
+                    System.out.println(s);
+                });
+
+        // lambda 3
+        Arrays.asList("a", "b", "c")
+                .forEach(s -> {
+                    System.out.println(s);
+                });
+
+        // lambda 4
+        Arrays.asList("a", "b", "c")
+                .forEach(s -> System.out.println(s));
+
+        // method reference
+        Arrays.asList("a", "b", "c")
+                .forEach(System.out::println);
 
     }
 
